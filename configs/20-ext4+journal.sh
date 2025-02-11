99f37c856cdc356d12d29de39b714d2849aff331
Eugene Chou <euchou@ucsc.edu>
2025-02-11 22:20:36 +0000

    only care about ext4 and dm-*

--- configs/20-ext4+journal.sh
+++ configs/20-ext4+journal.sh
@@ -8,27 +8,11 @@ if [ -z "${TEST_MNT:-}" ]; then echo "TEST_MNT: not specified. skipping..." >&2;
 CONFIG_NAME="$(basename "${BASH_SOURCE[0]%.sh}")"
 CONFIG_NUM="${CONFIG_NAME%%-*}"
 
-DISK1_INTEGRITY_NAME="disk1-integrity-${CONFIG_NUM}"
-DISK1_INTEGRITY_DEV="/dev/mapper/${DISK1_INTEGRITY_NAME}"
-
 function cmd_up() {
-    # Create and open dm-integrity (bitmap mode).
-    sudo integritysetup format \
-        --sector-size=4096 \
-        --batch-mode \
-        "${DISK1_DEV}"
-    sudo integritysetup open \
-        --integrity-bitmap-mode \
-        --bitmap-flush-time=5000 \
-        --batch-mode \
-        "${DISK1_DEV}" \
-        "${DISK1_INTEGRITY_NAME}"
-    sudo integritysetup status "${DISK1_INTEGRITY_NAME}"
-
     # Create and mount ext4.
-    sudo mkfs.ext4 -F "${DISK1_INTEGRITY_DEV}"
+    sudo mkfs.ext4 -F "${DISK1_DEV}"
     mkdir -p "${TEST_MNT}"
-    sudo mount -t ext4 "${DISK1_INTEGRITY_DEV}" "${TEST_MNT}"
+    sudo mount -t ext4 -o data=journal "${DISK1_DEV}" "${TEST_MNT}"
     sudo chmod a+rwx "${TEST_MNT}"
 
     # Print status.
@@ -39,7 +23,7 @@ function cmd_exec() {
     env \
         CONFIG_NAME="${CONFIG_NAME}" \
         CONFIG_NUM="${CONFIG_NUM}" \
-        TEST_DEV="${DISK1_INTEGRITY_DEV}" \
+        TEST_DEV="${DISK1_DEV}" \
         TEST_MNT="${TEST_MNT}" \
         "${@}"
 }
@@ -48,9 +32,6 @@ function cmd_down() {
     # Unmount ext4.
     ! mountpoint "${TEST_MNT}" || sudo umount "${TEST_MNT}"
     ! [ -e "${TEST_MNT}" ] || sudo rmdir --ignore-fail-on-non-empty -p "${TEST_MNT}"
-
-    # Close dm-integrity.
-    ! [ -e "${DISK1_INTEGRITY_DEV}" ] || sudo integritysetup close "${DISK1_INTEGRITY_NAME}"
 }
 
 "cmd_${1}" "${@:2}"
